91d1a25284a8426ec0a7d1369cd806b8c69e4937
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-09-26 18:15:58 +0500

    fix: fix

--- core/config/payme.py
+++ core/config/payme.py
@@ -1,12 +1,12 @@
 import os
 
 PAYME: dict = {
-    'PAYME_ID': str(os.getenv('PAYME_ID')),
-    'PAYME_KEY': str(os.getenv('PAYME_KEY')),
-    'PAYME_URL': str(os.getenv('PAYME_URL')),
-    'PAYME_CALL_BACK_URL': str(os.getenv('PAYME_CALL_BACK_URL')),
-    'PAYME_MIN_AMOUNT': int(os.getenv('PAYME_MIN_AMOUNT')),
-    'PAYME_ACCOUNT': str(os.getenv('PAYME_ACCOUNT')),
+    'PAYME_ID': os.getenv('PAYME_ID'),
+    'PAYME_KEY': os.getenv('PAYME_KEY'),
+    'PAYME_URL': os.getenv('PAYME_URL'),
+    'PAYME_CALL_BACK_URL': os.getenv('PAYME_CALL_BACK_URL'),
+    'PAYME_MIN_AMOUNT': os.getenv('PAYME_MIN_AMOUNT'),
+    'PAYME_ACCOUNT': os.getenv('PAYME_ACCOUNT'),
 }
 
 ORDER_MODEL = 'apps.ticket.Order'
