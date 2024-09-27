dc3ae5112a2743288ca22960af0bd60abb562a3e
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-09-27 15:39:52 +0500

    fix: fix

--- apps/payment/signals/payment.py
+++ apps/payment/signals/payment.py
@@ -2,9 +2,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from apps.payme.models import MerchantTransactionsModel
 from apps.payment.choices import PaymentChoices
 from apps.payment.models import Payment
 from apps.ticket.models import Donate, Order
+
+
+@receiver(post_save, sender=MerchantTransactionsModel)
+def check_payme_status(sender, instance, **kwargs):
+    if instance.state == int(1):
+        payment = Payment.objects.get(order_id=instance.order_id)
+        payment.status = PaymentChoices.COMPLETED
+        payment.save()
 
 
 @receiver(post_save, sender=Payment)
@@ -23,3 +32,10 @@ def check_payment_status(sender, instance, **kwargs):
             if order:
                 order.is_paid = True
                 order.save()
+
+
+@receiver(post_save, sender=Order)
+def check_order_status(sender, instance, **kwargs):
+    if instance.is_paid:
+        instance.seat.count -= instance.count
+        instance.seat.save()
