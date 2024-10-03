493953672f85955de7dcf256e615474d6c7a1640
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-03 11:34:24 +0500

    fix: translate fix

--- apps/shared/views.py
+++ apps/shared/views.py
@@ -6,15 +6,15 @@ class HomeView(TemplateView):
     template_name = "index.html"
 
 
-def handler404(request):  # noqa
+def handler404(request, exception):  # noqa
     return render(request, "exceptions/404.html", status=404)
 
 
-def handler400(request):  # noqa
+def handler400(request, exception):  # noqa
     return render(request, "exceptions/400.html", status=400)
 
 
-def handler403(request):  # noqa
+def handler403(request, exception):  # noqa
     return render(request, "exceptions/403.html", status=403)
 
 
@@ -22,5 +22,5 @@ def handler500(request):  # noqa
     return render(request, "exceptions/500.html", status=500)
 
 
-def handler502(request):  # noqa
+def handler502(request, exception):  # noqa
     return render(request, "exceptions/502.html", status=502)
