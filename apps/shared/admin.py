a6eed27639cdcd8735b3da3e1f4920215eaf4459
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-04 14:21:41 +0500

    fix: user admin fix

--- apps/shared/admin.py
+++ apps/shared/admin.py
@@ -1,9 +1,7 @@
-# Register your models here.
-
-
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group, User
-
 from unfold.admin import ModelAdmin
 from unfold.forms import UserChangeForm, UserCreationForm, AdminPasswordChangeForm
 
@@ -12,14 +10,14 @@ admin.site.unregister(User)
 
 
 @admin.register(Group)
-class GroupAdmin(ModelAdmin):
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
     filter_vertical = ("permissions",)
 
 
 @admin.register(User)
-class UserAdmin(ModelAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
     change_password_form = AdminPasswordChangeForm
     add_form = UserCreationForm
     form = UserChangeForm
@@ -28,18 +26,4 @@ class UserAdmin(ModelAdmin):
     search_fields = ("username", "email")
     ordering = ("username",)
     list_editable = ("is_active", "is_staff", "is_superuser")
-    actions = ["make_active", "make_inactive"]
     filter_vertical = ("groups", "user_permissions")
-
-    def make_active(self, request, queryset):  # noqa
-        queryset.update(is_active=True)
-
-    make_active.short_description = "Faol qilish"
-
-    def make_inactive(self, request, queryset):  # noqa
-        queryset.update(is_active=False)
-
-    make_inactive.short_description = "Faol emas qilish"
-
-    def has_add_permission(self, request):
-        return False
