3c4b37051fe489e245f5f5a6e0628eb6c8ed5078
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-05 18:05:50 +0500

    fix: fix

--- apps/ticket/utils/news.py
+++ apps/ticket/utils/news.py
@@ -1,12 +1,11 @@
 from celery import shared_task
-from celery.exceptions import MaxRetriesExceededError
 
 from apps.bot.utils.send_message import send_news
 from apps.ticket.models import News, BotUsers
 
 
-@shared_task(bind=True, max_retries=5, default_retry_delay=60)
-def send_news_to_subscribers(self, news_id):
+@shared_task()
+def send_news_to_subscribers(news_id):
     try:
         news = News.objects.get(id=news_id)
         users = BotUsers.objects.all()
@@ -15,7 +14,4 @@ def send_news_to_subscribers(self, news_id):
     except News.DoesNotExist:
         print(f"News with id {news_id} does not exist.")
     except Exception as exc:
-        try:
-            self.retry(exc=exc)
-        except MaxRetriesExceededError:
-            print(f"Max retries exceeded for news id {news_id}")
+        print(f"An error occurred: {exc}")
